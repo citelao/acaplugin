10da063f57f4c1952b1f3e7f55ec6301cd0cb1c9
Ben <ben@stolovitz.com>
2016-05-25 18:57:54 +0300

    composer adds CMB

--- acac-features.php
+++ acac-features.php
@@ -2,18 +2,17 @@
 /*
  Plugin Name: ACAC Featureset
  Plugin URI: TODO (github)
- Description: This internal plugin adds all the content features necessary to 
- 	run the ACAC site.
+ Description: This internal plugin adds all the content features necessary to run the ACAC site.
  Version: 0.0
  Author: Ben Stolovitz
  Author URI: http://ben.stolovitz.com
  License: Proprietary
  */
-
-require 'vendor/autoload.php';
+namespace Acaplugin;
+require_once('src/types.php');
 
 // Init
-add_action('init', 'aca_init');
-function aca_init() {
-	Acaplugin\Types::hella();
+add_action('init', 'Acaplugin\init');
+function init() {
+	Types\init();
 }
--- src/types.php
+++ src/types.php
@@ -1,8 +1,27 @@
 <?php
-namespace Acaplugin;
+namespace Acaplugin\Types;
 
-class Types {
-	static function hella() {
-		print("hellllllla");
-	}
+function init() {
+	register_auditionee();
+}
+
+function register_auditionee() {
+	$args = array(
+		'label' => 'Auditionee',
+		'description' => 'Auditionees are all the people who try out for groups',
+		'public' => false,
+		'exclude_from_search' => true,
+		'publicly_queryable' => false,
+		'show_ui' => true,
+		'show_in_menu' => true,
+		'menu_icon' => 'dashicons-smiley',
+		// 'capability_type' => 'auditionee'
+		// 'query_var'          => true,
+		// 'rewrite'            => array( 'slug' => 'book'),
+		
+		// 'has_archive'        => true,
+		// 'menu_position'      => null,
+		'supports' => array('revisions')
+	);
+	register_post_type('auditionee', $args);
 }
