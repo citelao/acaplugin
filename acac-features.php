16bbc43d5344a104b5b3dc8499d72353940bcca8
Ben <ben@stolovitz.com>
2016-05-26 16:35:23 +0300

    more robust dependency checking

--- acac-features.php
+++ acac-features.php
@@ -1,15 +1,22 @@
 <?php
 /*
- Plugin Name: ACAC Featureset
- Plugin URI: TODO (github)
- Description: This internal plugin adds all the content features necessary to run the ACAC site.
- Version: 0.0
- Author: Ben Stolovitz
- Author URI: http://ben.stolovitz.com
- License: Proprietary
- */
+Plugin Name: Acaplugin: the ACAC Featureset
+Plugin URI: TODO (github)
+Description: Add all content features necessary to run the ACAC site.
+Version: 0.0
+Author: Ben Stolovitz
+Author URI: http://ben.stolovitz.com
+License: Proprietary
+*/
+
 namespace Acaplugin;
-require __DIR__ . '/vendor/autoload.php';
+
+if( file_exists( __DIR__ . '/vendor/autoload.php' ) ) {
+	require __DIR__ . '/vendor/autoload.php';
+} else {
+	die( 'In order for Acaplugin to work, you must install its dependencies. '
+		. 'Please run the `make` command in ' . __DIR__ );
+}
 
 /**
  * Register types
